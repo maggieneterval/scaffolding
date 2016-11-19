61b78af10abf42a46ab6ef03fe3178b4803a92b3
Maggie Neterval <mneterval@gmail.com>
2016-11-19 18:31:50 -0500

    readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# Patient Questionnaire
+
+Live [here](https://patient-questionnaire.herokuapp.com/).
+
+Uses React, Redux, Redux Form, Rebass, Node, Express, and Sequelize.
--- db/index.js
+++ db/index.js
@@ -1,5 +1,6 @@
 const Sequelize = require('sequelize');
 
+//if not in production, run postgres locally and create db named 'patient_questionnaire'
 const db_url = process.env.DATABASE_URL || 'postgres://localhost:5432/patient_questionnaire';
 
 const db = new Sequelize(db_url, {
